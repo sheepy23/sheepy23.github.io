ac9db3cbaacfe2b1e19edd1af3b6c74891602832
yyyang <sheepy1023@gmail.com>
2022-10-24 01:53:58 -0700

    web v1.0

---
+++ Pei.js
@@ -0,0 +1,137 @@
+const myFunction0 = () => {
+  document.getElementById("JFK").style.display ='none'
+  document.getElementById("MFA").style.display ='none'
+  document.getElementById("CityHall").style.display ='none'
+  document.getElementById("GreenBuilding").style.display ='none'
+  document.getElementById("Wiesner").style.display ='none'
+  document.getElementById("Hancock").style.display ='none'
+  document.getElementById("IMPei").style.display ='block'
+}
+
+const myFunction1 = () => {
+  document.getElementById("JFK").style.display ='block'
+  document.getElementById("MFA").style.display ='none'
+  document.getElementById("CityHall").style.display ='none'
+  document.getElementById("GreenBuilding").style.display ='none'
+  document.getElementById("Wiesner").style.display ='none'
+  document.getElementById("Hancock").style.display ='none'
+  document.getElementById("IMPei").style.display ='none'
+}
+
+const myFunction2 = () => {
+  document.getElementById("JFK").style.display ='none'
+  document.getElementById("MFA").style.display ='block'
+  document.getElementById("CityHall").style.display ='none'
+  document.getElementById("GreenBuilding").style.display ='none'
+  document.getElementById("Wiesner").style.display ='none'
+  document.getElementById("Hancock").style.display ='none'
+  document.getElementById("IMPei").style.display ='none'
+}
+
+const myFunction3 = () => {
+  document.getElementById("JFK").style.display ='none'
+  document.getElementById("MFA").style.display ='none'
+  document.getElementById("CityHall").style.display ='block'
+  document.getElementById("GreenBuilding").style.display ='none'
+  document.getElementById("Wiesner").style.display ='none'
+  document.getElementById("Hancock").style.display ='none'
+  document.getElementById("IMPei").style.display ='none'
+}
+
+const myFunction4 = () => {
+  document.getElementById("JFK").style.display ='none'
+  document.getElementById("MFA").style.display ='none'
+  document.getElementById("CityHall").style.display ='none'
+  document.getElementById("GreenBuilding").style.display ='block'
+  document.getElementById("Wiesner").style.display ='none'
+  document.getElementById("Hancock").style.display ='none'
+  document.getElementById("IMPei").style.display ='none'
+}
+
+const myFunction5 = () => {
+  document.getElementById("JFK").style.display ='none'
+  document.getElementById("MFA").style.display ='none'
+  document.getElementById("CityHall").style.display ='none'
+  document.getElementById("GreenBuilding").style.display ='none'
+  document.getElementById("Wiesner").style.display ='block'
+  document.getElementById("Hancock").style.display ='none'
+  document.getElementById("IMPei").style.display ='none'
+}
+
+const myFunction6 = () => {
+  document.getElementById("JFK").style.display ='none'
+  document.getElementById("MFA").style.display ='none'
+  document.getElementById("CityHall").style.display ='none'
+  document.getElementById("GreenBuilding").style.display ='none'
+  document.getElementById("Wiesner").style.display ='none'
+  document.getElementById("Hancock").style.display ='block'
+  document.getElementById("IMPei").style.display ='none'
+}
+
+
+
+
+const textFunction0 = () => {
+  document.getElementById("JFKText").style.display ='none'
+  document.getElementById("MFAText").style.display ='none'
+  document.getElementById("CityHallText").style.display ='none'
+  document.getElementById("GreenBuildingText").style.display ='none'
+  document.getElementById("WiesnerText").style.display ='none'
+  document.getElementById("HancockText").style.display ='none'
+  document.getElementById("IMPeiText").style.display ='block'
+}
+const textFunction1 = () => {
+  document.getElementById("JFKText").style.display ='block'
+  document.getElementById("MFAText").style.display ='none'
+  document.getElementById("CityHallText").style.display ='none'
+  document.getElementById("GreenBuildingText").style.display ='none'
+  document.getElementById("WiesnerText").style.display ='none'
+  document.getElementById("HancockText").style.display ='none'
+  document.getElementById("IMPeiText").style.display ='none'
+}
+const textFunction2 = () => {
+  document.getElementById("JFKText").style.display ='none'
+  document.getElementById("MFAText").style.display ='block'
+  document.getElementById("CityHallText").style.display ='none'
+  document.getElementById("GreenBuildingText").style.display ='none'
+  document.getElementById("WiesnerText").style.display ='none'
+  document.getElementById("HancockText").style.display ='none'
+  document.getElementById("IMPeiText").style.display ='none'
+}
+const textFunction3 = () => {
+  document.getElementById("JFKText").style.display ='none'
+  document.getElementById("MFAText").style.display ='none'
+  document.getElementById("CityHallText").style.display ='block'
+  document.getElementById("GreenBuildingText").style.display ='none'
+  document.getElementById("WiesnerText").style.display ='none'
+  document.getElementById("HancockText").style.display ='none'
+  document.getElementById("IMPeiText").style.display ='none'
+}
+const textFunction4 = () => {
+  document.getElementById("JFKText").style.display ='none'
+  document.getElementById("MFAText").style.display ='none'
+  document.getElementById("CityHallText").style.display ='none'
+  document.getElementById("GreenBuildingText").style.display ='block'
+  document.getElementById("WiesnerText").style.display ='none'
+  document.getElementById("HancockText").style.display ='none'
+  document.getElementById("IMPeiText").style.display ='none'
+}
+const textFunction5 = () => {
+  document.getElementById("JFKText").style.display ='none'
+  document.getElementById("MFAText").style.display ='none'
+  document.getElementById("CityHallText").style.display ='none'
+  document.getElementById("GreenBuildingText").style.display ='none'
+  document.getElementById("WiesnerText").style.display ='block'
+  document.getElementById("HancockText").style.display ='none'
+  document.getElementById("IMPeiText").style.display ='none'
+}
+const textFunction6 = () => {
+  document.getElementById("JFKText").style.display ='none'
+  document.getElementById("MFAText").style.display ='none'
+  document.getElementById("CityHallText").style.display ='none'
+  document.getElementById("GreenBuildingText").style.display ='none'
+  document.getElementById("WiesnerText").style.display ='none'
+  document.getElementById("HancockText").style.display ='block'
+  document.getElementById("IMPeiText").style.display ='none'
+}
+
